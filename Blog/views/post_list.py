29ff43c659eeafbf840a9cacf8a3ebf31df2d46f
chenmich <403189920@qq.com>
2017-11-20 10:12:01 +0800

    temp storage

--- Blog/views/post_list.py
+++ Blog/views/post_list.py
@@ -6,5 +6,4 @@ from flask_login import current_user
 @app.route('/')
 def index():
     blog_views = get_post_views()
-    print(current_user)
     return render_template('post_list.html', blog_views=blog_views)
