4a80a746014387200a0c49a9e78dab548f9e8233
chenmich <403189920@qq.com>
2017-12-04 00:07:57 +0800

    tmp storage

--- Blog/views/post_editor.py
+++ Blog/views/post_editor.py
@@ -1,12 +1,25 @@
 from flask import render_template
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 from Blog import app
 from ..models import Post
 
 class PostEditor(FlaskForm):
-    pass
+    title = StringField('请输入文章标题', validators=[DataRequired()])
+    post_markdown_doc = TextAreaField()
+    post_html_code = TextAreaField()
+    publish = BooleanField('请求发布')
+    save = SubmitField('存储文章')
 
 @app.route('/editor/<post_title>')
 def post_editor(post_title):
-    return render_template('post_editor.html')
+    form = PostEditor()
+    
+    if form.validate_on_submit():
+        print(form.publish.data)
+        print("=====================================================")
+        
+
+    return render_template('post_editor.html', form=form)
