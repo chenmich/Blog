2d649d9c38c75371f7dae27bada003152d4b48f5
chenmich <403189920@qq.com>
2017-12-08 21:01:06 +0800

    add ajax for post the content of editor of posts

--- Blog/views/post_editor.py
+++ Blog/views/post_editor.py
@@ -1,20 +1,11 @@
-from flask import render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from flask import render_template, request
 
 from Blog import app
 from ..models import Post
 
-class PostEditor(FlaskForm):
-    title = StringField('文章标题', validators=[DataRequired()])
-    post_doc = TextAreaField(_name='post-markdown-doc')
-    post_html = TextAreaField(_name='post-html-code')
-    publish = BooleanField('请求发布')
-    save = SubmitField('存储文章')
+@app.route('/<post_title>', methods=['GET', 'POST'])
 
 @app.route('/editor/<post_title>')
 def post_editor(post_title):
-    form = PostEditor()
-    print(form.post_doc.data)
-    return render_template('post_editor.html', form=form)
+    print(request.args.get('a'))
+    return render_template('post_editor.html', post_title=post_title)
