aff1f995905e581da4eba0a2b601651503138df3
chenmich <403189920@qq.com>
2017-12-08 21:30:23 +0800

    modify ajax route to /<post_title>

--- Blog/views/post_editor.py
+++ Blog/views/post_editor.py
@@ -3,9 +3,11 @@ from flask import render_template, request
 from Blog import app
 from ..models import Post
 
-@app.route('/<post_title>', methods=['GET', 'POST'])
+@app.route('/<post_title>')
+def recieve_post(post_title):
+    print('ok')
+    return 'OK'
 
 @app.route('/editor/<post_title>')
-def post_editor(post_title):
-    print(request.args.get('markdownDoc'))
+def post_editor(post_title):    
     return render_template('post_editor.html', post_title=post_title)
