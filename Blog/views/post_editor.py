6d6994e8907d9266d00359c71fa3aff1cce731bd
chenmich <403189920@qq.com>
2017-12-10 12:00:34 +0800

    refactor the code of the post_editor template to js file

--- Blog/views/post_editor.py
+++ Blog/views/post_editor.py
@@ -2,14 +2,10 @@ from flask import render_template, request
 from Blog import app
 from ..models import Post
 
-@app.route('/<post_title>', methods=['POST'])
-def recieve_post(post_title):
-    if request.method == 'GET':
-        print('ok')
-    return 'OK'
+
 
 @app.route('/editor/<post_title>', methods=['POST', 'GET'])
 def post_editor(post_title):
     if request.method == 'POST':
-        pass
+        print(request.json['markdownDoc'])
     return render_template('post_editor.html', post_title=post_title)
