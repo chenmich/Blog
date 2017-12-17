054baf28b31049364e0456f247bb0327db90d29c
chenmich <403189920@qq.com>
2017-12-17 19:39:37 +0800

    complete client local storage markdown doc

--- Blog/static/editor/editor.js
+++ Blog/static/editor/editor.js
@@ -1,67 +1,10 @@
 webstorage_enabled = false
 var is_doc_posted = false 
-var markdown_doc = ''
-post_interval = 10*60*1000
-if (typeof(Storage) !== "undefined") {
-    webstorage_enabled = true    
-    $('div#webstorage-message').css('display','none')       
-}else
-    post_interval = 5*60*1000
+var post_interval = 12000 //10*60*1000
 
+var editor;
 //create editor instance
-var editor = editormd({
-    id   : "post",
-    path : "/static/editor.md/lib/",
-    width: 100%$,
-    height: 100%$,
-    theme : "dark",
-    
-    toolbarIcons : function() {
-        // Or return editormd.toolbarModes[name]; // full, simple, mini
-        // Using "||" set icons align right.
-        return editormd.toolbarModes['simple']
-    },
-    // toolbarIcons : "full", // You can also use editormd.toolbarModes[name] default list, values: full, simple, mini.
-    //previewTheme : "dark",
-    //editorTheme : "pastel-on-dark",
-    codeFold : true,
-    syncScrolling : true,
-    saveHTMLToTextarea : true,    // 保存 HTML 到 Textarea
-    searchReplace : true,
-    //watch : false,                // 关闭实时预览
-    htmlDecode : "style,script,iframe|on*",            // 开启 HTML 标签解析，为了安全性，默认不开启    
-    //toolbar  : false,             //关闭工具栏
-    //previewCodeHighlight : false, // 关闭预览 HTML 的代码块高亮，默认开启
-    emoji : true,
-    taskList : true,
-    tocm            : true,         // Using [TOCM]
-    tex : true,                   // 开启科学公式TeX语言支持，默认关闭
-    flowChart : true,             // 开启流程图支持，默认关闭
-    sequenceDiagram : true,       // 开启时序/序列图支持，默认关闭,
-    //dialogLockScreen : false,   // 设置弹出层对话框不锁屏，全局通用，默认为true
-    //dialogShowMask : false,     // 设置弹出层对话框显示透明遮罩层，全局通用，默认为true
-    //dialogDraggable : false,    // 设置弹出层对话框不可拖动，全局通用，默认为true
-    //dialogMaskOpacity : 0.4,    // 设置透明遮罩层的透明度，全局通用，默认值为0.1
-    //dialogMaskBgColor : "#000", // 设置透明遮罩层的背景颜色，全局通用，默认为#fff
-    imageUpload : true,
-    imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
-    imageUploadURL : "./php/upload.php",
-    onload : function() {
-        if(webstorage_enabled && localStorage.markdownDoc && 
-            localStorage.is_doc_posted === 'false' && localStorage.markdownDoc !== '')
-            if (confirm('你上次编辑的文档还没有上传，要恢复吗？')){
-                $('textarea#doc').text = editor.getMarkdown()
-            }
-    },
-    onchange: function(){
-        if(webstorage_enabled)
-            save_doc()
-        else{
-            post_doc()
-            localStorage.is_doc_posted = true
-        }
-    }
-});
+
 //post doc
 function post_doc(){
     $.ajax({
@@ -71,27 +14,86 @@ function post_doc(){
             markdownDoc: editor.getMarkdown() }, null, '\t'),
         success: function(data){
                         console.log(data)
+                        localStorage.is_doc_posted = true
                     },
         error: function(jqXhr, textStatus, errorThrown){
                         console.log(textStatus)
                 },
         datatype: 'json',
-        contentType: 'application/json'
-    
+        contentType: 'application/json'    
     });
 }
 function save_doc(){
-    if(webstorage_enabled){
         localStorage.markdownDoc = editor.getMarkdown()
-        localStorage.is_doc_posted = false
-    }
 }
 
 
-$(function(){    
+$(function(){
+    if (typeof(Storage) !== "undefined") {
+        webstorage_enabled = true    
+        $('div#webstorage-message').css('display','none')       
+    }else
+        post_interval = 5*60*1000
+    editor = editormd({
+        id   : "post",
+        path : "/static/editor.md/lib/",
+        width: 100%$,
+        height: 100%$,
+        theme : "dark",
+        
+        toolbarIcons : function() {
+            // Or return editormd.toolbarModes[name]; // full, simple, mini
+            // Using "||" set icons align right.
+            return editormd.toolbarModes['simple']
+        },
+        // toolbarIcons : "full", // You can also use editormd.toolbarModes[name] default list, values: full, simple, mini.
+        //previewTheme : "dark",
+        //editorTheme : "pastel-on-dark",
+        codeFold : true,
+        syncScrolling : true,
+        saveHTMLToTextarea : true,    // 保存 HTML 到 Textarea
+        searchReplace : true,
+        //watch : false,                // 关闭实时预览
+        htmlDecode : "style,script,iframe|on*",            // 开启 HTML 标签解析，为了安全性，默认不开启    
+        //toolbar  : false,             //关闭工具栏
+        //previewCodeHighlight : false, // 关闭预览 HTML 的代码块高亮，默认开启
+        emoji : true,
+        taskList : true,
+        tocm            : true,         // Using [TOCM]
+        tex : true,                   // 开启科学公式TeX语言支持，默认关闭
+        flowChart : true,             // 开启流程图支持，默认关闭
+        sequenceDiagram : true,       // 开启时序/序列图支持，默认关闭,
+        //dialogLockScreen : false,   // 设置弹出层对话框不锁屏，全局通用，默认为true
+        //dialogShowMask : false,     // 设置弹出层对话框显示透明遮罩层，全局通用，默认为true
+        //dialogDraggable : false,    // 设置弹出层对话框不可拖动，全局通用，默认为true
+        //dialogMaskOpacity : 0.4,    // 设置透明遮罩层的透明度，全局通用，默认值为0.1
+        //dialogMaskBgColor : "#000", // 设置透明遮罩层的背景颜色，全局通用，默认为#fff
+        imageUpload : true,
+        imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+        imageUploadURL : "./php/upload.php",
+        onload : function() {
+            if(webstorage_enabled && localStorage.markdownDoc && 
+                localStorage.is_doc_posted === 'false' && localStorage.markdownDoc !== '')
+                if (confirm('你上次编辑的文档还没有上传，要恢复吗？')){
+                    editor.setMarkdown(localStorage.markdownDoc)
+                }
+        },
+        onchange: function(){
+            if(webstorage_enabled){
+                save_doc()
+                localStorage.is_doc_posted = false
+            }
+            else{
+                post_doc()
+            }
+        }
+    });
+
     setInterval(post_doc, post_interval)
-    window.onbeforeunload = function(e){
-        if(webstorage_enabled && localStorage.is_doc_posted === 'false')
-            post_doc()
-    }
 })
+
+window.onbeforeunload = function(e){
+    if(webstorage_enabled && localStorage.is_doc_posted === 'false')
+        post_doc()
+}
+
