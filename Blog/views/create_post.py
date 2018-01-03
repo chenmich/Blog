50421b7d080d7c6afd640aa84905aebc2721f720
chenmich <403189920@qq.com>
2018-01-03 19:05:56 +0800

    tmp storage

--- Blog/views/create_post.py
+++ Blog/views/create_post.py
@@ -8,15 +8,17 @@ from Blog import app
 class PostAttributeForm(FlaskForm):
     post_title = StringField('博文标题')
     abstract = TextAreaField('摘要')
-    other_writers = SelectMultipleField('其它作者')
     publish = BooleanField('发布')
-    other_writers = SelectMultipleField(u'Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
+    other_writers = SelectMultipleField('其它作者', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
     submit_post = SubmitField('提交')
 
 @app.route('/create_post', methods=['post', 'get'])
 def create_post():
     form = PostAttributeForm()
+    form.abstract.data='''###摘要
+    '''
+    
     if request.method == 'POST':
         print('The method is post')
-        print(form.post_title.data)
+        print(form.other_writers.data)
     return render_template('create_post.html', form=form)
