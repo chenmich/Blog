fa5f54c1e2e5d300682ff5509023d4e53fd76150
chenmich <403189920@qq.com>
2018-01-08 22:07:14 +0800

    tmp storage

--- Blog/views/create_post.py
+++ Blog/views/create_post.py
@@ -3,22 +3,27 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextAreaField, SelectMultipleField, SubmitField
 from wtforms.validators import DataRequired
 from flask_login import current_user
-from Blog import app
+from Blog import app, db
+from ..models import User
+
+def getAllUserIdentifiers():
+    users = User.query.all()
+    users_identifiers = [(str(user.id), user.username) for user in users]
+    return users_identifiers
+
 
 class PostAttributeForm(FlaskForm):
     post_title = StringField('博文标题', validators=[DataRequired()])
     abstract = TextAreaField('摘要')
     publish = BooleanField('发布')
-    other_writers = SelectMultipleField('其它作者', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
+    other_writers = SelectMultipleField('其它作者', choices=getAllUserIdentifiers())
     submit_post = SubmitField('提交')
 
 @app.route('/create_post', methods=['post', 'get'])
 def create_post():
-    form = PostAttributeForm()
-    if form.validate_on_submit():
-        print('The method is post')
-        print(form.other_writers.data)
+    form = PostAttributeForm()    
+    if form.validate_on_submit():        
         return 'ok'
     else:
-        print(form.errors)
+        print(form.post_title.errors)
         return render_template('create_post.html', form=form)
