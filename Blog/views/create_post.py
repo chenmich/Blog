0b403e11767138609a98ad5053fd50660b69513f
chenmich <403189920@qq.com>
2018-01-09 16:43:25 +0800

    tmp storage

--- Blog/views/create_post.py
+++ Blog/views/create_post.py
@@ -4,15 +4,17 @@ from wtforms import StringField, BooleanField, TextAreaField, SelectMultipleFiel
 from wtforms.validators import DataRequired
 from flask_login import current_user
 from Blog import app, db
-from ..models import User
+from ..models import User, Post, Post_User
 
 def getAllUserIdentifiers():
     users = User.query.all()
     users_identifiers = [(str(user.id), user.username) for user in users]
     return users_identifiers
 
-def _create_post_entity(username, post_title, other_writers):
-    pass
+def _create_post_entity(userId, post_title, other_writers_id):
+    first_writer = User.query.all().filter_by(id=userId)
+    other_writers = [User.query.all().filter_by(id=int(_id)) for _id in other_writers_id]
+    post = Post(post_title=post_title)
 
 class PostAttributeForm(FlaskForm):
     post_title = StringField('博文标题', validators=[DataRequired()])
@@ -25,8 +27,9 @@ class PostAttributeForm(FlaskForm):
 def create_post():
     form = PostAttributeForm()    
     if form.validate_on_submit():
-        _create_post_entity(current_user.username, form.post_title, form.other_writers)    
+        _create_post_entity(current_user.id, form.post_title, form.other_writers)    
         return 'ok'
     else:
 
         return render_template('create_post.html', form=form)
+
