4ee2493d3592a0af14c42630d66b9dd6b92ca769
chenmich <403189920@qq.com>
2018-01-16 17:24:34 +0800

    tmp storage

--- Blog/views/post_editor.py
+++ Blog/views/post_editor.py
@@ -1,12 +1,24 @@
-from flask import render_template, request
-from Blog import blog_blue
+from flask import render_template, request, redirect, url_for, current_app
+from flask_login import login_required, current_user
+from Blog import blog_blue, login_manager
 from ..models import Post
 
-
+def can_be_modify_by_current_user(post_title):
+    post = Post.query.filter_by(title=post_title).first()
+    if post is None: 
+        return False
+    first_writer = post.first_writer
+    if (first_writer.id == current_user.id): 
+        return True
+    else:
+        return False
 
 @blog_blue.route('/editor/<post_title>', methods=['POST', 'GET'])
+@login_required
 def post_editor(post_title):
+    if can_be_modify_by_current_user(post_title) is False:
+        return current_app.login_manager.unauthorized()
     if request.method == 'POST':
         print(request.json['markdownDoc'])
-        return 'OK'
+        return 'ok'
     return render_template('post_editor.html', post_title=post_title)
