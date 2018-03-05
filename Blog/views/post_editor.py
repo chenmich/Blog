405bfed87041817a166cc0309bfea493da65f5da
chenmich <403189920@qq.com>
2018-03-05 10:11:31 +0800

    modify a name of function to set it a private

--- Blog/views/post_editor.py
+++ Blog/views/post_editor.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from Blog import blog_blue, login_manager
 from ..models import Post
 
-def can_be_modify_by_current_user(post_title):
+def __can_be_modify_by_current_user__(post_title):
     post = Post.query.filter_by(title=post_title).first()
     if post is None: 
         return False
@@ -16,7 +16,7 @@ def can_be_modify_by_current_user(post_title):
 @blog_blue.route('/editor/<post_title>', methods=['POST', 'GET'])
 @login_required
 def post_editor(post_title):
-    if can_be_modify_by_current_user(post_title) is False:
+    if __can_be_modify_by_current_user__(post_title) is False:
         return current_app.login_manager.unauthorized()
     if request.method == 'POST':
         print(request.json['markdownDoc'])
